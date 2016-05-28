df02b8eaac118415bcdfd2db12d20b5903100093
teor (Tim Wilson-Brown) <teor2345@gmail.com>
2016-05-29 06:20:34 +1000

    Initial Project Template
    Modified slightly from the Apple Swift template.

---
+++ SwiftBigInt/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SwiftBigInt
+//
+//  Created by Tim Wilson-Brown on 29/05/2016.
+//  Copyright © 2016 teor.
+//  GPL version 3 or later.
+//
+
+import Foundation
+
+print("Hello, Big Integers!")
+
